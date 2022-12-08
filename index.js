3cc22d5c3e2834995bc135941c8b2451b7a16031
B0bbycode <onaolapolekki@outlook.com>
2022-12-08 22:19:38 +0200

    new commit for EdithAi

---
+++ index.js
@@ -0,0 +1,48 @@
+// Import the required libraries
+const express = require('express');
+const dialogflow = require('dialogflow');
+
+// Set up the Dialogflow client
+const client = new dialogflow.v2beta1.SessionsClient({
+    credentials: {
+        client_email: '<client_email>',
+        private_key: '<private_key>'
+    }
+});
+const sessionPath = client.sessionPath('<project_id>', '<session_id>');
+
+// Set up the Express app
+const app = express();
+
+// Route to handle incoming messages
+app.post('/message', (req, res) => {
+    // Get the user's message from the request body
+    const message = req.body.message;
+
+    // Detect the user's intent using Dialogflow
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,
+                languageCode: 'en-US'
+            }
+        }
+    };
+
+    // Generate a response from Dialogflow
+    client.detectIntent(request)
+        .then(response => {
+            const result = response[0].queryResult;
+            // Return the AI response to the user
+            res.send(result.fulfillmentText);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
+
+// Start the Express app
+app.listen(3000, () => {
+    console.log('Chat bot listening on port 3000');
+});
